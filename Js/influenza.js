33f020b366cfb5928a98314a6d9e500b1c1e7722
Muqeet <sukhan34@gmail.com.com>
2025-09-29 14:26:40 +0500

    lulu

--- Js/influenza.js
+++
@@ -1,6 +0,0 @@
- document.querySelectorAll(".accordion-header").forEach(button => {
-    button.addEventListener("click", () => {
-      const item = button.parentElement;
-      item.classList.toggle("active");
-    });
-  });
